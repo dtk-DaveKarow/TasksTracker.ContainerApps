a23d0414c213a5f2f4f8b505f9bc72b18c9526c4
Dave Karow <dtk@davekarow.com>
2024-01-08 14:45:32 -0800

    Part 2: Commented out stuff introduced in later tutorials and uncommented Fake

--- TasksTracker.TasksManager.Backend.Api/Program.cs
+++ TasksTracker.TasksManager.Backend.Api/Program.cs
@@ -1,23 +1,23 @@
-using Dapr.Client;
-using Microsoft.ApplicationInsights.Extensibility;
-using TasksTracker.TasksManager.Backend.Api;
+//using Dapr.Client;
+//using Microsoft.ApplicationInsights.Extensibility;
+//using TasksTracker.TasksManager.Backend.Api;
 using TasksTracker.TasksManager.Backend.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<DaprClient>(_ => new DaprClientBuilder().Build());
+//builder.Services.AddSingleton<DaprClient>(_ => new DaprClientBuilder().Build());
 
-//builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
+builder.Services.AddSingleton<ITasksManager, FakeTasksManager>();
 
-builder.Services.AddSingleton<ITasksManager, TasksStoreManager>();
+//builder.Services.AddSingleton<ITasksManager, TasksStoreManager>();
 
 builder.Services.AddControllers();
 
-builder.Services.AddApplicationInsightsTelemetry();
+//builder.Services.AddApplicationInsightsTelemetry();
 
-builder.Services.Configure<TelemetryConfiguration>((o) => {
+//builder.Services.Configure<TelemetryConfiguration>((o) => {
     o.TelemetryInitializers.Add(new AppInsightsTelemetryInitializer());
 });
 
